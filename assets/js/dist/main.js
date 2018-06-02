685f604d1bee8d6de2c2354c63443922770002ff
Austin Pickett <pickett.austin@gmail.com>
2018-06-02 19:38:03 -0400

    Fix skybox alignment, generate dynamic terrain from height map, remove sphere

--- assets/js/dist/main.js
+++ assets/js/dist/main.js
@@ -20,13 +20,8 @@ class WebGLScene {
     this.camera = null;
     this.groundMaterial = null;
     this.terrain = null;
-    this.onKeyDown = this.onKeyDown.bind(this);
-    this.onKeyUp = this.onKeyUp.bind(this);
     this.run = this.run.bind(this);
     this.createScene = this.createScene.bind(this);
-    this.initGame = this.initGame.bind(this);
-    document.addEventListener("keydown", this.onKeyDown);
-    document.addEventListener("keyup", this.onKeyUp);
     window.addEventListener('resize', this.engine.resize());
     this.run();
   }
@@ -34,14 +29,58 @@ class WebGLScene {
   createScene() {
     const scene = new BABYLON.Scene(this.engine);
     scene.enablePhysics();
-    scene.collisionsEnabled = true; //this.camera = new BABYLON.ArcRotateCamera('camera', -(Math.PI / 2), 1.0, 30, this.scene)
-
-    this.camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -30), scene);
-    this.camera.checkCollisions = true; //this.camera.applyGravity = true
-
-    this.camera.attachControl(canvas, true);
-    const light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0, 100, -50), this.scene);
+    scene.collisionsEnabled = true;
+    scene.gravity = new BABYLON.Vector3(0, -9.81, 0);
+    scene.fogMode = BABYLON.Scene.FOGMODE_LINEAR;
+    scene.fogDensity = .001;
+    scene.fogColor = new BABYLON.Color3(0, 0, 0);
+    this.camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 10, -30), scene);
+    this.camera.checkCollisions = true;
+    this.camera.applyGravity = true;
+    this.camera.attachControl(this.canvas, true);
+    const light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0, 100, -50), scene);
     light.intensity = 0.7;
+    const groundMaterial = new BABYLON.GridMaterial('groundMat', scene);
+    groundMaterial.majorUnitFrequency = 5;
+    groundMaterial.minorUnitVisibility = 0.45;
+    groundMaterial.gridRatio = 1;
+    groundMaterial.backFaceCulling = false;
+    groundMaterial.mainColor = new BABYLON.Color3(0, 0, 0);
+    groundMaterial.lineColor = new BABYLON.Color3(255, 0, 144);
+    groundMaterial.opacity = .9; // callback function : terrain creation
+
+    var mapSubX = 1000;
+    var mapSubZ = 800;
+    var terrain;
+
+    var createTerrain = function (mapData, mapSubX, mapSubZ) {
+      var params = {
+        mapData: mapData,
+        mapSubX: mapSubX,
+        mapSubZ: mapSubZ,
+        terrainSub: 120
+      };
+      terrain = new BABYLON.DynamicTerrain("terrain", params, scene);
+      terrain.createUVMap();
+      terrain.mesh.material = groundMaterial;
+      terrain.mesh.checkCollisions = true;
+      terrain.update(true);
+    };
+
+    var hmURL = "assets/textures/heightmap.jpg";
+    var mapWidth = 1000;
+    var mapHeight = 1000;
+    var nbPoints = 500;
+    var hmOptions = {
+      width: mapWidth,
+      height: mapHeight,
+      subX: nbPoints,
+      subZ: nbPoints,
+      onReady: createTerrain
+    };
+    var mapData = new Float32Array(nbPoints * nbPoints * 3); // the array that will store the generated data
+
+    BABYLON.DynamicTerrain.CreateMapFromHeightMapToRef(hmURL, hmOptions, mapData, scene);
     return scene;
   }
 
@@ -63,93 +102,9 @@ class WebGLScene {
       task.loadedMeshes[0].position = new BABYLON.Vector3(-25, 20, 100);
     };
 
-    this.assetsManager.onFinish = () => {
-      _this.initGame();
-
-      _this.engine.runRenderLoop(() => _this.scene.render());
-    };
+    this.assetsManager.onFinish = () => _this.engine.runRenderLoop(() => _this.scene.render());
 
     this.assetsManager.load();
-  }
-
-  initGame() {
-    const sphereMat = new BABYLON.StandardMaterial('sphereMaterial', this.scene);
-    sphereMat.diffuseColor = new BABYLON.Color3(1, 144, 244);
-    this.sphere = new BABYLON.Mesh.CreateSphere('sphere1', 16, 1, this.scene);
-    this.sphere.position.y = 10;
-    this.sphere.material = sphereMat;
-    this.sphere.checkCollisions = true;
-    this.sphere.ellipsoid = new BABYLON.Vector3(1, 1, 1);
-    this.sphere.physicsImpostor = new BABYLON.PhysicsImpostor(this.sphere, BABYLON.PhysicsImpostor.SphereImpostor, {
-      mass: 1,
-      friction: 0.3,
-      restitution: 0
-    }, this.scene); //this.camera.setTarget(this.sphere)
-
-    const groundMaterial = new BABYLON.GridMaterial('groundMat', this.scene);
-    groundMaterial.majorUnitFrequency = 5;
-    groundMaterial.minorUnitVisibility = 0.45;
-    groundMaterial.gridRatio = 1;
-    groundMaterial.backFaceCulling = false;
-    groundMaterial.mainColor = new BABYLON.Color3(0, 0, 0);
-    groundMaterial.lineColor = new BABYLON.Color3(255, 0, 144);
-    groundMaterial.opacity = .9;
-    const mapSubX = 100,
-          mapSubZ = 100,
-          terrainSub = 50;
-    const mapData = new Float32Array(mapSubX * mapSubZ * 3);
-
-    for (var l = 0; l < mapSubZ; l++) {
-      for (var w = 0; w < mapSubX; w++) {
-        mapData[3 * (l * mapSubX + w)] = (w - mapSubX * 0.5) * 2.0;
-        mapData[3 * (l * mapSubX + w) + 1] = 0;
-        mapData[3 * (l * mapSubX + w) + 2] = (l - mapSubZ * 0.5) * 2.0;
-      }
-    }
-
-    const params = {
-      mapData,
-      mapSubX,
-      mapSubZ,
-      terrainSub
-    };
-    const terrain = new BABYLON.DynamicTerrain("terrain", params, this.scene);
-    terrain.mesh.position.y = -1.0;
-    terrain.mesh.material = groundMaterial;
-    terrain.mesh.checkCollisions = true;
-    terrain.mesh.physicsImpostor = new BABYLON.PhysicsImpostor(terrain.mesh, BABYLON.PhysicsImpostor.BoxImpostor, {
-      mass: 0,
-      friction: 0.3,
-      restitution: 0.9
-    }, this.scene);
-  }
-
-  onKeyUp() {
-    this.sphere.physicsImpostor.applyImpulse(new BABYLON.Vector3(0, 0, 0), this.sphere.position);
-  }
-
-  onKeyDown(event) {
-    switch (event.keyCode) {
-      case 87:
-        this.sphere.physicsImpostor.applyImpulse(new BABYLON.Vector3(0, 0, 1), this.sphere.position);
-        break;
-
-      case 65:
-        this.sphere.physicsImpostor.applyImpulse(new BABYLON.Vector3(-1, 0, 0), this.sphere.position);
-        break;
-
-      case 83:
-        this.sphere.physicsImpostor.applyImpulse(new BABYLON.Vector3(0, 0, -1), this.sphere.position);
-        break;
-
-      case 68:
-        this.sphere.physicsImpostor.applyImpulse(new BABYLON.Vector3(1, 0, 0), this.sphere.position);
-        break;
-
-      case 32:
-        this.sphere.physicsImpostor.applyImpulse(new BABYLON.Vector3(0, 10, 0), this.sphere.position);
-        break;
-    }
   }
 
 }
